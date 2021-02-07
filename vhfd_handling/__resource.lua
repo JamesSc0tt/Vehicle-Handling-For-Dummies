862975cdddbceb6c7984f9bfffaaba9a06066bf7
James Lee Scott <james.lee.scott@icloud.com>
2021-02-07 11:32:58 +0000

    oops forgot this

--- vhfd_handling/__resource.lua
+++ vhfd_handling/__resource.lua
@@ -2,18 +2,18 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 
 dependency "vhfd_builders"
-fsn_handling "src/muscle.lua"
-fsn_handling "src/compact.lua"
-fsn_handling "src/coupes.lua"
-fsn_handling "src/sports.lua"
-fsn_handling "src/suvs.lua"
-fsn_handling "src/sportsclassics.lua"
-fsn_handling "src/offroad.lua"
-fsn_handling "src/vans.lua"
-fsn_handling "src/sedans.lua"
-fsn_handling "src/government.lua"
-fsn_handling "src/super.lua"
---fsn_handling "src/motorcycles.lua"
+vhfd_handling "src/muscle.lua"
+vhfd_handling "src/compact.lua"
+vhfd_handling "src/coupes.lua"
+vhfd_handling "src/sports.lua"
+vhfd_handling "src/suvs.lua"
+vhfd_handling "src/sportsclassics.lua"
+vhfd_handling "src/offroad.lua"
+vhfd_handling "src/vans.lua"
+vhfd_handling "src/sedans.lua"
+vhfd_handling "src/government.lua"
+vhfd_handling "src/super.lua"
+--vhfd_handling "src/motorcycles.lua"
 
 files { 'out/handling.meta', 'data/handling.meta' }
 data_file 'HANDLING_FILE' 'out/handling.meta'
